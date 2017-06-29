d0288bd2eda29325891ee572da8b5bc035455a55
Martin Valgur <martin.valgur@gmail.com>
2017-06-29 16:38:19 +0300

    Fix tests unintentionally modifying the socket status for other tests

--- tests/test_socket.py
+++ tests/test_socket.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
 import pytest
+from pytest_socket import enable_socket
+
+
+@pytest.fixture(autouse=True)
+def reenable_socket():
+    # The tests can leave the socket disabled in the global scope.
+    # Fix that by automatically re-enabling it after each test
+    yield
+    enable_socket()
 
 
 def assert_socket_blocked(result):
     result.stdout.fnmatch_lines("""
         *SocketBlockedError: A test tried to use socket.socket.*
     """)
+
+
+def test_socket_enabled_by_default(testdir):
+    testdir.makepyfile("""
+        import socket
+        
+        def test_socket():
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    """)
+    result = testdir.runpytest("--verbose")
+    result.assert_outcomes(1, 0, 0)
+    with pytest.raises(Exception):
+        assert_socket_blocked(result)
 
 
 def test_global_disable_via_fixture(testdir):
@@ -93,8 +115,7 @@ def test_urllib_succeeds_by_default(testdir):
             assert urlopen('https://httpbin.org/get').getcode() == 200
     """)
     result = testdir.runpytest("--verbose")
-    result.assert_outcomes(0, 0, 1)
-    assert_socket_blocked(result)
+    result.assert_outcomes(1, 0, 0)
 
 
 def test_enabled_urllib_succeeds(testdir):
