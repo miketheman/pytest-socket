b679e281cbfb378e01b4bdf822a66a080ea27012
Mike Fiedler <miketheman@gmail.com>
2019-01-07 19:51:54 -0500

    :sparkles: pytest-socket, v0.3.2
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-socket',
-    version='0.3.1',
+    version='0.3.2',
     author='Mike Fiedler',
     author_email='miketheman@gmail.com',
     maintainer='Mike Fiedler',
