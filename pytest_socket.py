83b596abc2be676dd458fb744afb303eb7f0b161
Mike Fiedler <miketheman@gmail.com>
2021-12-23 14:57:24 -0500

    fix: test allow_hosts for unix sockets (#93)

--- pytest_socket.py
+++ pytest_socket.py
@@ -56,6 +56,15 @@ def socket_enabled(pytestconfig):
     yield
 
 
+def _is_unix_socket(family) -> bool:
+    try:
+        is_unix_socket = family == socket.AF_UNIX
+    except AttributeError:
+        # AF_UNIX not supported on Windows https://bugs.python.org/issue33408
+        is_unix_socket = False
+    return is_unix_socket
+
+
 def disable_socket(allow_unix_socket=False):
     """ disable socket.socket to disable the Internet. useful in testing.
     """
@@ -63,13 +72,7 @@ def disable_socket(allow_unix_socket=False):
     class GuardedSocket(socket.socket):
         """ socket guard to disable socket creation (from pytest-socket) """
         def __new__(cls, family=-1, type=-1, proto=-1, fileno=None):
-            try:
-                is_unix_socket = family == socket.AF_UNIX
-            except AttributeError:
-                # AF_UNIX not supported on Windows https://bugs.python.org/issue33408
-                is_unix_socket = False
-
-            if is_unix_socket and allow_unix_socket:
+            if _is_unix_socket(family) and allow_unix_socket:
                 return super().__new__(cls, family, type, proto, fileno)
 
             raise SocketBlockedError()
@@ -130,7 +133,7 @@ def _resolve_allow_hosts(item):
         hosts = mark_restrictions.args[0]
     elif cli_restrictions:
         hosts = cli_restrictions
-    socket_allow_hosts(hosts)
+    socket_allow_hosts(hosts, allow_unix_socket=item.config.__socket_allow_unix_socket)
     return hosts
 
 
@@ -151,7 +154,7 @@ def host_from_connect_args(args):
         return host_from_address(address)
 
 
-def socket_allow_hosts(allowed=None):
+def socket_allow_hosts(allowed=None, allow_unix_socket=False):
     """ disable socket.socket.connect() to disable the Internet. useful in testing.
     """
     if isinstance(allowed, str):
@@ -161,8 +164,9 @@ def socket_allow_hosts(allowed=None):
 
     def guarded_connect(inst, *args):
         host = host_from_connect_args(args)
-        if host and host in allowed:
+        if host in allowed or (_is_unix_socket(inst.family) and allow_unix_socket):
             return _true_connect(inst, *args)
+
         raise SocketConnectBlockedError(allowed, host)
 
     socket.socket.connect = guarded_connect
--- tests/test_combinations.py
+++ tests/test_combinations.py
@@ -1,4 +1,6 @@
 """Test module to express odd combinations of configurations."""
+# TODO: Move this to a more common location, like `conftest.py`.
+from test_async import unix_sockets_only
 
 
 def test_parametrize_with_socket_enabled_and_allow_hosts(testdir, httpbin):
@@ -45,3 +47,31 @@ def test_parametrize_with_socket_enabled_and_allow_hosts(testdir, httpbin):
     result.stdout.fnmatch_lines(
         "*SocketConnectBlockedError: A test tried to use socket.socket.connect() with host*"
     )
+
+
+@unix_sockets_only
+def test_combine_unix_and_allow_hosts(testdir, httpbin):
+    """Test combination of disable, allow-unix and allow-hosts.
+
+    From https://github.com/miketheman/pytest-socket/issues/78
+    """
+    testdir.makepyfile(
+        f"""
+        import socket
+
+        import pytest
+
+
+        def test_unix_connect():
+            sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            with pytest.raises(FileNotFoundError):
+                sock.connect('/tmp/socket.sock')
+
+
+        def test_inet_connect():
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(('{httpbin.host}', {httpbin.port}))
+        """
+    )
+    result = testdir.runpytest("--disable-socket", "--allow-unix-socket", f"--allow-hosts={httpbin.host}")
+    result.assert_outcomes(passed=2)
