4d5f8efcc52d5177e605e68e83577b919e49acbb
Tom Forbes <tom@tomforb.es>
2022-01-20 19:41:39 +0000

    Support non-function test items in pytest_runtest_setup
    https://github.com/pytest-dev/pytest/issues/5070 for more information

--- pytest_socket.py
+++ pytest_socket.py
@@ -113,6 +113,11 @@ def pytest_runtest_setup(item) -> None:
     As the logic can be extensive, this method is allowed complexity.
     It may be refactored in the future to be more readable.
     """
+       
+    # If the given item is not a function test (i.e a DoctestItem) or otherwise 
+    # has no support for fixtures, skip
+    if not hasattr(item, "fixturenames"):
+        return
 
     # If test has the `enable_socket` marker, we accept this as most explicit.
     if "socket_enabled" in item.fixturenames or item.get_closest_marker(
