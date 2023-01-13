893803353b9ccc775ba112c531e17a94aaf5a9f5
Martín Gaitán <gaitan@gmail.com>
2023-01-13 14:16:07 -0300

    flake8

--- pytest_socket.py
+++ pytest_socket.py
@@ -199,15 +199,15 @@ def _remove_restrictions():
 def is_valid_host(host, allowed):
     if not host:
         return False
-    
+
     ips = [ip for ip in allowed if "/" not in ip]
     if host in ips:
         return True
 
     networks = [ipaddress.ip_network(mask) for mask in allowed if "/" in mask]
-    if not networks: 
+    if not networks:
         return False
-        
+
     ip = ipaddress.ip_address(host)
     for net in networks:
         if ip in net:
