25ea24684d187623d659b5b1825cc584a28e8cba
Mike Fiedler <miketheman@gmail.com>
2023-06-18 14:25:43 -0400

    test: extract common function for reuse
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- tests/test_precedence.py
+++ tests/test_precedence.py
@@ -1,16 +1,7 @@
 """Test module to express precedence tests between the different
 configuration combinations"""
 
-
-def assert_socket_blocked(result, passed=0, skipped=0, failed=1):
-    """Uses built in methods to test for common failure scenarios.
-    Usually we only test for a single failure,
-    but sometimes we want to test for multiple conditions,
-    so we can pass in the expected counts."""
-    result.assert_outcomes(passed=passed, skipped=skipped, failed=failed)
-    result.stdout.fnmatch_lines(
-        "*Socket*Blocked*Error: A test tried to use socket.socket.*"
-    )
+from tests.common import assert_socket_blocked
 
 
 def test_disable_via_fixture(testdir):
